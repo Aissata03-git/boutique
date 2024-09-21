1ae5501fb763217a927b599565abaffbafcb7303
Aissata03-git <diarraaissata990@gmail.com>
2024-09-21 16:12:14 +0000

    init projet

---
+++ src/main/java/com/example/gestionboutiq/GestionboutiqApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionboutiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionboutiqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionboutiqApplication.class, args);
+	}
+
+}
